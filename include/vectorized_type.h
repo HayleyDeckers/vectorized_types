a279174b6e9d88a325da0f426207dcbe7932e6e3
Roel Deckers <git@codingcat.nl>
2017-12-08 15:58:53 +0100

    CMake support

--- include/vectorized_type.h
+++ include/vectorized_type.h
@@ -1,9 +1,11 @@
 #ifndef VECTORIZED_TYPE_HPP
 #define VECTORIZED_TYPE_HPP
 
+//Basic definition and fallback type.
 #include "vectorized_type/default.h"
 #include <immintrin.h>
 
+//pick the largest available vector register as the default.
 #ifdef __AVX512F__
 #include "vectorized_type/preferences/avx512.h"
 #elif __AVX__
---
+++ include/vectorized_type/preferences/sse.h
@@ -0,0 +1,104 @@
+#ifndef VECT_PREF_SSE_H
+#define VECT_PREF_SSE_H
+#include "../default.h"
+#include "mathfun/sse_mathfun.h"
+#include <cstdint>
+namespace vec{
+  template<>
+  struct preffered_vector_type<int8_t>{
+    constexpr static int width = 16;
+    using type = int8_t  __attribute__ ((vector_size (16)));
+
+  };
+
+  template<>
+  struct preffered_vector_type<uint8_t>{
+    constexpr static int width = 16;
+    using type = uint8_t  __attribute__ ((vector_size (16)));
+
+  };
+
+  template<>
+  struct preffered_vector_type<int32_t>{
+    constexpr static int width = 4;
+    using type = int32_t  __attribute__ ((vector_size (16)));
+
+  };
+
+  template<>
+  struct preffered_vector_type<uint32_t>{
+    constexpr static int width = 4;
+    using type = uint32_t  __attribute__ ((vector_size (16)));
+
+  };
+
+  template<>
+  struct preffered_vector_type<float>{
+    constexpr static int width = 4;
+    using type = float  __attribute__ ((vector_size (16)));
+
+  };
+
+  template<>
+  struct preffered_vector_type<int64_t>{
+    constexpr static int width = 2;
+    using type = int64_t  __attribute__ ((vector_size (16)));
+
+  };
+
+  template<>
+  struct preffered_vector_type<uint64_t>{
+    constexpr static int width = 2;
+    using type = uint64_t  __attribute__ ((vector_size (16)));
+
+  };
+
+  template<>
+  struct preffered_vector_type<double>{
+    constexpr static int width = 2;
+    using type = double  __attribute__ ((vector_size (16)));
+
+  };
+
+
+  template<>
+  inline vectorized_type<float>::vectorized_type(const float* val){
+    mVal = _mm_loadu_ps(val);
+  }
+  template<>
+  inline vectorized_type<double>::vectorized_type(const double* val){
+    mVal = _mm_loadu_pd(val);
+  }
+
+  template<>
+  inline void vectorized_type<float>::set_1(float val){
+    mVal = _mm_set1_ps(val);
+  }
+  template<>
+  inline void vectorized_type<double>::set_1(double val){
+    mVal = _mm_set1_pd(val);
+  }
+
+  template<>
+  inline vectorized_type<float> vectorized_type<float>::sqrt() const{
+    return _mm_sqrt_ps(mVal);
+  }
+  template<>
+  inline vectorized_type<double> vectorized_type<double>::sqrt() const{
+    return _mm_sqrt_pd(mVal);
+  }
+
+  template<>
+  inline vectorized_type<float> vectorized_type<float>::sin() const{
+    return internal::sin_ps(mVal);
+  }
+  template<>
+  inline vectorized_type<float> vectorized_type<float>::cos() const{
+    return internal::cos_ps(mVal);
+  }
+  template<>
+  inline vectorized_type<float> vectorized_type<float>::log() const{
+    return internal::log_ps(mVal);
+  }
+}
+#endif
